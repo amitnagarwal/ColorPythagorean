d90037fddfa471f5230276169a5a2b9b7553a646
Amit Agarwal <anagarwal@yahoo.com>
2019-11-26 22:12:40 -0800

    Add new functions to make the code modular

--- PythagoreanTriple.h
+++ PythagoreanTriple.h
@@ -17,8 +17,10 @@ public:
 	bool operator==(const PythogoreanTriple& pt) const;
 
 	int getLegIndex(long long nodeIndex) const;
+	bool isSingleColored(const vector<Color>& colors) const;
 	bool isColoredValidly(const vector<Color>& colors) const;
 	bool isColoredValidlyOnFirstTwo(const vector<Color>& colors) const;
+	short getNumUnassigned(const vector<Color>& colors) const;
 	void handleSingleUnassigned(vector<Color>& inColors) const;
 	void handleDoubleUnassigned(vector<Color>& inColors) const;
 	bool allColorsDifferentHandled(vector<Color>& inColors) const;
